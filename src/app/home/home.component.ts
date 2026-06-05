2e1368b2c87cabbbe5ce09e20e5b2549cdafd18c
Daniel Abrahao <daniel@preface.com.br>
2026-06-05 16:09:56 -0300

    Ajustes de warnings

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
